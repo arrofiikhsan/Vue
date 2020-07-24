d599a6337572b56fbe318f0ed2fe022ea9edde34
smk_telkom <arrofiikhsan28@gmail.com>
2020-07-24 09:09:25 +0700

    commit 2

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-// var app = new Vue({
-//     el: '#app',
-//     data :{
-//         'message' :'hello world'
-//     }
-
-// });
-var app = new Vue ({
-    el :"#app",
-    methods :{
-        greet: function(name){
-            return 'Good morning'+name;
-        }
+var app = new Vue({
+    el: '#app',
+    data :{
+        'message' :'hello world'
     }
-})
+
+});
